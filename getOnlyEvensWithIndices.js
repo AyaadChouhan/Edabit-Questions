171cc1dbde450fe168787dd85229a138a7c75f35
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-06-05 00:33:41 +0530

    Add files via upload

---
+++ getOnlyEvensWithIndices.js
@@ -0,0 +1,13 @@
+// Given an array of numbers, return an array which contains all the even numbers in the original array, which also have even indices.
+function getOnlyEvensWithIndices(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0 && i % 2 === 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(getOnlyEvensWithIndices([1, 3, 2, 6, 4, 8]));
+console.log(getOnlyEvensWithIndices([0, 1, 2, 3, 4]));
+console.log(getOnlyEvensWithIndices([1, 2, 3, 4, 5]));
