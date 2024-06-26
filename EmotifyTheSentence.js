fcf578e79b7697319304fcff7343a663c6365055
Ayaad Chouhan <136963590+AyaadChouhan@users.noreply.github.com>
2024-06-26 23:28:58 +0530

    Add files via upload

---
+++ EmotifyTheSentence.js
@@ -0,0 +1,16 @@
+// function emotify(str) {
+//   const emoticons = {
+//     smile: ":D",
+//     grin: ":)",
+//     sad: ":(",
+//     mad: ":P",
+//   };
+//   return str
+//     .split("")
+//     .map((word) => emoticons[word] || word)
+//     .join("");
+// }
+// console.log(emotify("make me smile"));
+// console.log(emotify("make me grin"));
+
+
---
+++ checkValues.js
@@ -0,0 +1,6 @@
+// check the both values a and b are equal then return true else return false
+
+function and(a, b) {
+    return (a && b === true) ? true : false
+}
+console.log(and(false, true))
